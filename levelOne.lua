f9eecf321b918207cfae6ac4eb8817a9ae24354a
Drew Hunt <21234303@shrewsbury.ac.uk>
2023-03-27 21:05:19 +0100

    Added beta sprite physics

--- levelOne.lua
+++ levelOne.lua
@@ -1,38 +1,43 @@
 levelOne = {}
 
 require("level_oneButtons")
+require("levelOneMap")
 
 -- Load the sprite image
-local spriteImage = love.graphics.newImage("iamtesting.png")
+spriteImage = love.graphics.newImage("iamtesting.png")
 
 -- Define the sprite position
-local spriteX, spriteY
+spriteX, spriteY = 50
 
 -- Define the sprite dimensions
-local spriteWidth, spriteHeight = spriteImage:getDimensions()
+spriteWidth, spriteHeight = spriteImage:getDimensions()
 
 --Theres are used to store the velocitys of the sprite for both x and y
-local spriteVelX, spriteVelY = 0
+spriteVelX, spriteVelY = 0
 
-local spriteMaxVelPosX = 100
+spriteMaxVelPosX = 100
 
-local spriteMaxVelNegX = -100
+spriteMaxVelNegX = -100
 
-local floorFriction = 20
+floorFriction = 20
 
-local gravityStrenth = 10 
+gravityStrenth = 10 
 
-local jumpVel = 100
+jumpVel = 100
 
 -- Where the map begins to draw
-local mapX, mapY = 0, 0
+mapX, mapY = 0, 0
 
 -- Might be used for fall damage
-local momentumX, momentumY = 0
+momentumX, momentumY = 0
 
-local acceleration = 20
+acceleration = 20
 
-local bool collisionLeft, collisionRight, collisionUp, collisionDown  = False
+collisionLeft, collisionRight, collisionUp, collisionDown  = false
+
+VelocityDecayTimer = 0.00001
+
+VelocityDecayConstant = 500
 
 collisionTileWidth, collisionTileHeight = 64, 64
 
@@ -40,7 +45,7 @@ spriteX, spriteY = 64, 64
 
 
 
-function game_Level_one:enter()
+function levelOne:enter()
     print("Starting LevelOne Enter tasks")
     makeLevelOneButtonClickable()
     print ("Finsished LevelOne Enter Tasks")
@@ -48,48 +53,99 @@ function game_Level_one:enter()
 
 end
     
-function game_Level_one:exit()
+function levelOne:exit()
     print ("Starting LevelOne exit tasks")
     makeLevelOneButtonUnclickable()
     print ("finishing LevelOne exit tasks")
 end
 
 
-function game_Level_one:update(dt)
+function levelOne:update(dt)
     timer = timer + dt
+    VelocityDecayTimer = VelocityDecayTimer + dt
+    
     if handleKeyPress("escape") then
         changeState(Menu)
-        
     end
+    collisionLeft, collisionRight, collisionUp, collisionDown  = false
     SpriteVelocityCalculator()
-
-
-
-
-    
-    -- Check for collisions
+    SpriteVelocityDecay()
+    print (ve)
 
 end
 
-function SpriteVelocityCalculator(spriteX, spriteY, spriteVelX, spriteVelY, spriteMaxVelPosX, spriteMaxVelNegX)
+
+
+
+
+function SpriteMover()
+
+    
+
+
+end
+
+function SpriteVelocityDecay()
+    if love.keyboard.isDown("left") then 
+        print("Left arrow key inputting movement, not decaying velocity")
+        VelocityDecayTimer = 0
+    else
+        if love.keyboard.isDown("right") then
+            print("right arrow key inputting movement, not decaying velocity")
+            VelocityDecayTimer = 0
+        else
+            print("no input on x axis, decaying velocity")
+            if VelocityDecayTimer > VelocityDecayConstant then
+              
+                if spriteVelX > 0 then
+                    if spriteVelX - floorFriction < 0 then
+                        spriteVelX = 0    
+                    elseif spriteVelX - floorFriction > 0 then
+                            spriteVelX = spriteVelX - floorFriction      
+                    else 
+                            print("ERROR - SpriteVelocityDecay had a condition that should not be possible")  
+                    end
+                end
+                if spriteVelX < 0 then
+                    if spriteVelX + floorFriction > 0 then
+                        spriteVelX = 0
+                    elseif spriteVelX + floorFriction < 0 then
+                        spriteVelX = spriteVelX + floorFriction
+                    else
+                        print("ERROR - SpriteVelocityDecay had a condition that should not be possible") 
+                    end
+                end
+                VelocityDecayTimer = 0
+            end
+        end
+    end
+end
+
+
+
+
+
+
+
+function SpriteVelocityCalculator()
     if love.keyboard.isDown("right") and not collisionRight then
         
-        if spriteVelX + acceleration < spriteMaxVelPosY then 
+        if (spriteVelX + acceleration) < spriteMaxVelPosX then 
             spriteVelX = spriteVelX + acceleration 
             print ("spriteVelX increaced to", spriteVelX)
 
         else
-            spriteVelX = spriteMaxVelPosY
+            spriteVelX = spriteMaxVelPosX
             print ("spriteVelX has reached spriteMaxVelPosX")
         end
     end
 
-    if love.keyboard.isDown("left") then
-        if spriteVelX + acceleration < spriteMaxVelNegY then
+    if love.keyboard.isDown("left") and not collisionLeft then
+        if spriteVelX + acceleration < spriteMaxVelNegX then
             spriteVelX = spriteVelX - acceleration 
             print ("spriteVelX decreced to", spriteVelX)
         else
-            spriteVelX = spriteMaxVelNegY
+            spriteVelX = spriteMaxVelNegX
             print ("spriteVelX has reached spriteMaxVelNegX")
         end
     end
@@ -100,6 +156,44 @@ function SpriteVelocityCalculator(spriteX, spriteY, spriteVelX, spriteVelY, spri
         end
     end
 
+end
+
+
+function SpriteCollisionChecker()
+    -- Determine the indices of the collision tiles that the sprite intersects with
+    local spriteLeftIndex = math.floor(spriteX / collisionTileWidth) + 1
+    local spriteRightIndex = math.floor((spriteX + spriteWidth) / collisionTileWidth) + 1
+    local spriteTopIndex = math.floor(spriteY / collisionTileHeight) + 1
+    local spriteBottomIndex = math.floor((spriteY + spriteHeight) / collisionTileHeight) + 1
+    
+    local collisionLeft, collisionRight, collisionUp, collisionDown = false, false, false, false
+    
+    -- Check if the sprite intersects with any solid tiles
+    for y = spriteTopIndex, spriteBottomIndex do
+        for x = spriteLeftIndex, spriteRightIndex do
+            if levelOneMap_collisions[y][x] == 1 then
+                -- Check which side the collision occurred on
+                local tileX = (x - 1) * collisionTileWidth
+                local tileY = (y - 1) * collisionTileHeight
+                
+                if spriteX + spriteWidth > tileX and spriteX < tileX then
+                    print("Collision Left")
+                    collisionLeft = true
+                elseif spriteX < tileX + collisionTileWidth and spriteX + spriteWidth > tileX + collisionTileWidth then
+                    print("Collision Right")
+                    collisionRight = true
+                end
+                
+                if spriteY + spriteHeight > tileY and spriteY < tileY then
+                    print("Collision Up")
+                    collisionUp = true
+                elseif spriteY < tileY + collisionTileHeight and spriteY + spriteHeight > tileY + collisionTileHeight then
+                    print("Collision Down")
+                    collisionDown = true
+                end
+            end
+        end
+    end
 end
     
 
@@ -129,7 +223,7 @@ local mapWidth, mapHeight = #map[1], #map
 
 
 
-function game_Level_one:draw()
+function levelOne:draw()
     -- Draw the map
     for y = 1, mapHeight do
         for x = 1, mapWidth do
@@ -142,91 +236,4 @@ end
 
 
 
-function checkCollisions()
-    -- Determine the indices of the collision tiles that the sprite intersects with
-    local spriteLeftIndex = math.floor(spriteX / collisionTileWidth) + 1
-    local spriteRightIndex = math.floor((spriteX + spriteWidth) / collisionTileWidth) + 1
-    local spriteTopIndex = math.floor(spriteY / collisionTileHeight) + 1
-    local spriteBottomIndex = math.floor((spriteY + spriteHeight) / collisionTileHeight) + 1
-    
-    -- Check if the sprite intersects with any solid tiles
-    for y = spriteTopIndex, spriteBottomIndex do
-        for x = spriteLeftIndex, spriteRightIndex do
-            if levelOneMap_collisions[y][x] == 1 then
-                return true
-            end
-        end
-    end
-    
-    -- If the sprite does not intersect with any solid tiles, return false
-    
-    return false
-end
-local map = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-}
 
-local levelOneMap_collisions = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-}
-
-local levelOneMap_interactions = {
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-}
